026b07b7a086d5f28c9d6d9ecc9b9ee19437ebc8
RediiChen <a0929052958@gmail.com>
2021-03-22 21:51:56 +0800

    匯入內容＆建立樣式

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 var docLink = "https://datacenter.taichung.gov.tw/Swagger/OpenData/2072c001-c06e-4276-bbf1-84f3b113c38c?limit=100" ;
 const doc = {
     data() {
-        return {}
+        return {
+            docs : ""
+        }
     },
     mounted() {
+        //以下為在Vue框架中操作AJAX的流程
         var xhr = new XMLHttpRequest() ;
         xhr.open("GET", docLink) ;
         xhr.send() ;
         xhr.onload = () => {
             var json = xhr.responseText ;
+            var obj = JSON.parse(json) ;
+            this.docs = obj ;
+            console.log(obj) ;//暫時查看方便
         }
     }
 } ;
